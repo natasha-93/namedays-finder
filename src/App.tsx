58fe12c1611296dd4641f8d4f7fdbab1b1cfdfce
Natasha Lawson <natasha.lawson93@gmail.com>
2020-04-27 17:49:04 -0600

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import styles from "./App.module.css";
+
+const formatDate = (date: Date) => {
+  const year = date.getFullYear();
+  const month = `${date.getMonth() + 1}`.padStart(2, "0");
+  const day = `${date.getDate()}`.padStart(2, "0");
+
+  return [year, month, day].join("-");
+};
+
+const isValidDateStr = (date: string) => !Number.isNaN(Date.parse(date));
 
 function App() {
+  const [countryOptions, setCountryOptions] = useState(["us"]);
+  const [country, setCountry] = useState(countryOptions[0]);
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [namedays, setNamedays] = useState([]);
+
+  useEffect(() => {
+    const month = selectedDate.getMonth() + 1;
+    const day = selectedDate.getDate();
+
+    if (Number.isNaN(month) || Number.isNaN(day)) return;
+
+    fetch(`https://api.abalin.net/namedays?month=${month}&day=${day}`)
+      .then((response) => {
+        return response.json();
+      })
+      .then(({ data }) => {
+        setCountryOptions(Object.keys(data.namedays));
+        setNamedays(data.namedays[country].split(", "));
+      });
+  }, [selectedDate, country]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Name Days Finder</h2>
+      <p className={styles.dayLabels}>
+        Names for:
+        {/* {["Yesterday", "Today", "Tomorrow"].map((day) => (
+          <button
+            className={clsx(styles.dayButton, {
+              [styles.selected]: day.toLowerCase() === selectedDay,
+            })}
+            key={day}
+            onClick={(e) => setSelectedDay(day.toLowerCase())}
+          >
+            {day}
+          </button>
+        ))} */}
+        <input
+          type="date"
+          value={formatDate(selectedDate)}
+          onChange={(e) => {
+            if (isValidDateStr(e.target.value)) {
+              setSelectedDate(new Date(e.target.value));
+            }
+          }}
+        />
+      </p>
+      <ul>
+        {namedays.map((name, index) => (
+          <li key={index}>{name}</li>
+        ))}
+      </ul>
+
+      <select value={country} onChange={(e) => setCountry(e.target.value)}>
+        {countryOptions.map((country) => (
+          <option key={country} value={country}>
+            {country}
+          </option>
+        ))}
+      </select>
     </div>
   );
 }
